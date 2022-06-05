bb1be9fe37b9c768ec23d3e2392dd2181fe8c36f
ricardojosino <ricardojosino@gmail.com>
2022-06-05 17:26:18 -0300

    Começando a LP

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [],
+  theme: {
+    color: {
+      'color01' : '#6666FF',
+      'color02' : '#FF3399',
+      'color03' : '#343434',
+      'color04' : '#F4F4F4',
+      'color05' : '#F3F3F3',
+      'color06' : '#FFFFFF',
+    },
+
+    extend: {
+      screen : {
+        'xl' : '1200px'
+      },
+    },
+  },
+  plugins: [],
+}
